49d97da6589979841f2403048fcab8abe40b9beb
sajithakkr <saji.krishnanivas@gmail.com>
2022-04-06 20:56:52 +0530

    7th code commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var os = require('os');
 var http = require('http');
 
+//constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
 function handleRequest(req, res) {
   res.write('Hi there! I\'m being served from ' + os.hostname())
   res.end()
 };
 
-http.createServer(handleRequest).listen(5000, '54.204.84.38');
+http.createServer(handleRequest).listen(PORT, HOST);
